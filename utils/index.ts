0e91c9f9de3e3f397c146d6a5bc4c69e0f904713
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2020-01-17 21:40:50 +0900

    fix: format repo name

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,4 @@
 
 export const getRepoENSName = (repo: any) => `${repo.name.toLowerCase()}.${repo.owner.login.toLowerCase()}.qqq.eth`;
 
-export const getDeployedURL = (user: TUser, repo: any) => `http://${repo.name.toLowerCase()}.${user.login.toLowerCase()}.qqq.poseidon.work`;
+export const getDeployedURL = (user: TUser, repo: any) => `http://${repo.name.toLowerCase().replace(/[.]/g, '-')}.${user.login.toLowerCase()}.qqq.poseidon.work`;
